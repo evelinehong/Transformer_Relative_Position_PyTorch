9befc894caea65e0ecc272743ad28718e74f581b
Yining Hong <yninghong@gmail.com>
2021-01-07 14:46:27 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Transformer_Relative_Position_Self_Attention
- Implement the paper "Self-Attention with Relative Position Representations"
+Implement the paper ["Self-Attention with Relative Position Representations"](https://arxiv.org/pdf/1803.02155.pdf) \
+For the entire Seq2Seq framework, you can refer to this [repo](https://github.com/evelinehong/hint-baselines/tree/master/cnn-transformer-relative-position).
---
+++ relative_position.py
@@ -0,0 +1,97 @@
+class RelativePosition(nn.Module):
+
+    def __init__(self, num_units, max_relative_position):
+        super().__init__()
+        self.num_units = num_units
+        self.max_relative_position = max_relative_position
+        self.embeddings_table = nn.Parameter(torch.Tensor(max_relative_position * 2 + 1, num_units))
+        nn.init.xavier_uniform_(self.embeddings_table)
+
+    def forward(self, length_q, length_k):
+        range_vec_q = torch.arange(length_q)
+        range_vec_k = torch.arange(length_k)
+        distance_mat = range_vec_k[None, :] - range_vec_q[:, None]
+        distance_mat_clipped = torch.clamp(distance_mat, -self.max_relative_position, self.max_relative_position)
+        final_mat = distance_mat_clipped + self.max_relative_position
+        final_mat = torch.LongTensor(final_mat).cuda()
+        embeddings = self.embeddings_table[final_mat].cuda()
+
+        return embeddings
+
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, dropout, device):
+        super().__init__()
+        
+        assert hid_dim % n_heads == 0
+        
+        self.hid_dim = hid_dim
+        self.n_heads = n_heads
+        self.head_dim = hid_dim // n_heads
+        self.max_relative_position = 2
+
+        self.relative_position_k = RelativePosition(self.head_dim, self.max_relative_position)
+        self.relative_position_v = RelativePosition(self.head_dim, self.max_relative_position)
+
+        self.fc_q = nn.Linear(hid_dim, hid_dim)
+        self.fc_k = nn.Linear(hid_dim, hid_dim)
+        self.fc_v = nn.Linear(hid_dim, hid_dim)
+        
+        self.fc_o = nn.Linear(hid_dim, hid_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        self.scale = torch.sqrt(torch.FloatTensor([self.head_dim])).to(device)
+        
+    def forward(self, query, key, value, mask = None):
+        #query = [batch size, query len, hid dim]
+        #key = [batch size, key len, hid dim]
+        #value = [batch size, value len, hid dim]
+        batch_size = query.shape[0]
+        len_k = key.shape[1]
+        len_q = query.shape[1]
+        len_v = value.shape[1]
+
+        query = self.fc_q(query)
+        key = self.fc_k(key)
+        value = self.fc_v(value)
+
+        r_q1 = query.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        r_k1 = key.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        attn1 = torch.matmul(r_q1, r_k1.permute(0, 1, 3, 2)) 
+
+        r_q2 = query.permute(1, 0, 2).contiguous().view(len_q, batch_size*self.n_heads, self.head_dim)
+        r_k2 = self.relative_position_k(len_q, len_k)
+        attn2 = torch.matmul(r_q2, r_k2.transpose(1, 2)).transpose(0, 1)
+        attn2 = attn2.contiguous().view(batch_size, self.n_heads, len_q, len_k)
+        attn = (attn1 + attn2) / self.scale
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e10)
+
+        attn = self.dropout(torch.softmax(attn, dim = -1))
+
+        #attn = [batch size, n heads, query len, key len]
+        r_v1 = value.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        weight1 = torch.matmul(attn, r_v1)
+        r_v2 = self.relative_position_v(len_q, len_v)
+        weight2 = attn.permute(2, 0, 1, 3).contiguous().view(len_q, batch_size*self.n_heads, len_k)
+        weight2 = torch.matmul(weight2, r_v2)
+        weight2 = weight2.transpose(0, 1).contiguous().view(batch_size, self.n_heads, len_q, self.head_dim)
+
+        x = weight1 + weight2
+        
+        #x = [batch size, n heads, query len, head dim]
+        
+        x = x.permute(0, 2, 1, 3).contiguous()
+        
+        #x = [batch size, query len, n heads, head dim]
+        
+        x = x.view(batch_size, -1, self.hid_dim)
+        
+        #x = [batch size, query len, hid dim]
+        
+        x = self.fc_o(x)
+        
+        #x = [batch size, query len, hid dim]
+        
+        return x
